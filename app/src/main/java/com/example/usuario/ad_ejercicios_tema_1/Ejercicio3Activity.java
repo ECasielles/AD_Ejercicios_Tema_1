593b32ea4d19bdb01276e304b9cce954c1e07f06
ECasielles <flapspoiler@gmail.com>
2017-09-28 14:29:49 +0200

    1st commit

---
+++ app/src/main/java/com/example/usuario/ad_ejercicios_tema_1/Ejercicio3Activity.java
@@ -0,0 +1,13 @@
+package com.example.usuario.ad_ejercicios_tema_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ejercicio3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio3);
+    }
+}
---
+++ app/src/main/java/com/example/usuario/ad_ejercicios_tema_1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.usuario.ad_ejercicios_tema_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.btnEj1:
+                intent = new Intent(MainActivity.this, Ejercicio1Activity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnEj2:
+                intent = new Intent(MainActivity.this, Ejercicio2Activity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnEj3:
+                intent = new Intent(MainActivity.this, Ejercicio3Activity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnEj4:
+                intent = new Intent(MainActivity.this, Ejercicio4Activity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
