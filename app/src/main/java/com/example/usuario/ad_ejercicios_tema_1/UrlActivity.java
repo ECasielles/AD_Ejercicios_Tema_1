6e83078dc85385a4d090a6e39360cdfd7d3661c0
ECasielles <flapspoiler@gmail.com>
2017-10-09 12:58:27 +0200

    hotfix2

--- app/src/main/java/com/example/usuario/ad_ejercicios_tema_1/UrlActivity.java
+++ app/src/main/java/com/example/usuario/ad_ejercicios_tema_1/UrlActivity.java
@@ -3,6 +3,7 @@ package com.example.usuario.ad_ejercicios_tema_1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class UrlActivity extends AppCompatActivity {
 
@@ -15,6 +16,7 @@ public class UrlActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         String url = bundle.getString("url");
         mmiWV = (WebView) findViewById(R.id.miWV);
+        mmiWV.setWebViewClient(new WebViewClient());
         mmiWV.loadUrl(url);
     }
 }
